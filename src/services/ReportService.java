5351e18688d2da8aa6978d67ff25e4f8209d6f59
Soumya Varshney <soumya.varshney.btech2024@sitpune.edu.in>
2026-04-16 14:12:02 +0530

    ReportService.java implemented

--- src/services/ReportService.java
+++ src/services/ReportService.java
@@ -1,24 +1,36 @@
+// ReportService.java
 package services;
- 
 import database.ReportDAO;
+import database.LocationDAO;
 import models.Report;
 import java.util.*;
 
 public class ReportService {
+    private ReportDAO reportDAO = new ReportDAO();
+    private LocationDAO locationDAO = new LocationDAO();
 
-    private ReportDAO dao = new ReportDAO();
-
-    public void addReport(int userId, String desc) {
-        Report r = new Report(userId, desc);
-        dao.addReport(r);
+    public void addReport(int userId, String desc, String type, String evidence, String[] loc) {
+    try {
+        int locationId = locationDAO.getOrCreateLocation(loc[0], loc[1], loc[2]);
+        Report r = new Report();
+        r.setUserId(userId);
+        r.setDescription(desc);
+        r.setType(type);
+        r.setEvidence(evidence);
+        r.setLocationId(locationId);
+        reportDAO.addReport(r);
+        System.out.println("  Report filed successfully.");
+    } catch (Exception e) {
+        System.out.println("  Failed to file report: " + e.getMessage());
     }
+}
 
     public void viewReports(int userId) {
-        List<Report> list = dao.getReports(userId);
-
+        List<Report> list = reportDAO.getReports(userId);
         for (Report r : list) {
             System.out.println(
                 r.getReportId() + " | " +
+                r.getType() + " | " +
                 r.getDescription()
             );
         }
