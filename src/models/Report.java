d278a38c13a9b710252b02d95a48bfb55970e42c
suchita-malde <suchitamalde04@gmail.com>
2026-04-16 14:00:58 +0530

    Update Report.java

--- src/models/Report.java
+++ src/models/Report.java
@@ -3,8 +3,11 @@ package models;
 public class Report {
     private int reportId;
     private int userId;
+    private int locationId;
+    private String type;
     private String description;
- 
+    private String evidence;
+
     public Report() {}
 
     public Report(int userId, String description) {
@@ -14,9 +17,15 @@ public class Report {
 
     public int getReportId() { return reportId; }
     public int getUserId() { return userId; }
+    public int getLocationId() { return locationId; }
+    public String getType() { return type; }
     public String getDescription() { return description; }
+    public String getEvidence() { return evidence; }
 
     public void setReportId(int reportId) { this.reportId = reportId; }
     public void setUserId(int userId) { this.userId = userId; }
+    public void setLocationId(int locationId) { this.locationId = locationId; }
+    public void setType(String type) { this.type = type; }
     public void setDescription(String description) { this.description = description; }
+    public void setEvidence(String evidence) { this.evidence = evidence; }
 }
