c37e90aa4844b95733a855a3e885f24a3faa1607
SohamS710 <sohamshinde07102@gmail.com>
2026-04-17 09:31:14 +0530

    update reportDAO.java

--- src/database/ReportDAO.java
+++ src/database/ReportDAO.java
@@ -6,14 +6,19 @@ import java.util.*;
 
 public class ReportDAO {
 
+
     public void addReport(Report report) {
-        String sql = "INSERT INTO Reports(user_id, description) VALUES (?, ?)";
+
+        String sql = "INSERT INTO Incident_Report (user_id, location_id, type, description, evidence) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ps.setInt(1, report.getUserId());
-            ps.setString(2, report.getDescription());
+            ps.setInt(2, report.getLocationId());   
+            ps.setString(3, report.getType());      
+            ps.setString(4, report.getDescription());
+            ps.setString(5, report.getEvidence());  
 
             ps.executeUpdate();
             System.out.println("Report Added!");
@@ -23,9 +28,11 @@ public class ReportDAO {
         }
     }
 
+
     public List<Report> getReports(int userId) {
+
         List<Report> list = new ArrayList<>();
-        String sql = "SELECT * FROM Reports WHERE user_id=?";
+        String sql = "SELECT * FROM Incident_Report WHERE user_id=?";
 
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -35,9 +42,14 @@ public class ReportDAO {
 
             while (rs.next()) {
                 Report r = new Report();
+
                 r.setReportId(rs.getInt("report_id"));
                 r.setUserId(rs.getInt("user_id"));
+                r.setLocationId(rs.getInt("location_id"));   
+                r.setType(rs.getString("type"));             
                 r.setDescription(rs.getString("description"));
+                r.setEvidence(rs.getString("evidence"));     
+
                 list.add(r);
             }
 
